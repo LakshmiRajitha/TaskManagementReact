d03eb82ec000cd30ef8d957fa50ea4cc2d3d9fd8
LakshmiRajitha <raji.ch2@gmail.com>
2023-03-26 22:04:11 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import TaskDashboard from "./components/TaskDashboard";
+import { GetTasks } from "../src/services/Task";
+
+function App() {
+  const [items, setitems] = useState([]);
+  const [renderUI, setrenderUI] = useState(false);
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  let getTasks = async () => {
+    let res = await GetTasks();
+    if (res) setitems(res);
+    setrenderUI(true);
+  };
+
+  return (
+    <>
+      {renderUI && (
+        <div className="App">
+          <TaskDashboard items={items}> </TaskDashboard>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ src/test/TaskDashboard.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import TaskDashboard from "../components/TaskDashboard";
+
+test("renders App", () => {
+  render(<TaskDashboard />);
+});
